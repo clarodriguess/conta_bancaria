410beb6cf66072d50e083987d6c28108d030dca0
clarisse <clarissebleasby@gmail.com>
2026-02-03 17:40:01 -0300

    Criaçao da classe main - menu

---
+++ Menu.ts
@@ -0,0 +1,82 @@
+import leia from 'readline-sync'
+
+export function main(){
+    let opcao:number
+
+    while(true){
+        console.log("*****************************************")
+        console.log("                                         ")
+        console.log("        BANCO GENERATION BRASIL          ")
+        console.log("                                         ")
+        console.log("*****************************************")
+        console.log("                                         ")
+        console.log("1 - Criar Conta                          ")
+        console.log("2 - Listar todas as contas               ")
+        console.log("3 - Buscar conta por numero              ")
+        console.log("4 - Atualizar dados da conta             ")
+        console.log("5 - Apagar conta                         ")
+        console.log("6 - Sacar                                ")
+        console.log("7 - Depositar                            ")
+        console.log("8 - Transferir                           ")
+        console.log("9 - Sair                                 ")
+        console.log("                                         ")
+        console.log("*****************************************")
+        console.log("                                         ")
+
+        console.log("Entre com a opção desejada: ")
+        opcao = leia.questionInt()
+
+        if(opcao == 9){
+            console.log("\nSaindo do sistema...")
+            console.log("Banco Generation Brasil - Seu futuro começa aqui!")
+            sobre()
+            process.exit(0)
+        }
+
+        switch(opcao){
+            case 1:
+                console.log("Criar conta")
+                
+                break
+            case 2:
+                console.log("Listar todas as contas")
+                
+                break
+            case 3:
+                console.log("Consultar conta - por numero")
+                
+                break
+            case 4:
+                console.log("Atualizar dados da conta")
+                
+                break
+            case 5:
+                console.log("Apagar uma conta")
+               
+                break
+            case 6:
+                console.log("Saque")
+                break
+            case 7:
+                console.log("Deposito")
+                
+                break
+            case 8:
+                console.log("Transferencia entre contas")
+               
+                break
+            default:
+                console.log("Opção inválida")
+        }
+    }
+}
+export function sobre():void{
+    console.log("\n******************************************************")
+    console.log("Projeto desenvolvido por: ")
+    console.log("Clarisse Rodrigues")
+    console.log("clarissebleasby@gmail.com")
+    console.log("https://github.com/clarodriguess")
+    console.log("********************************************************")
+}
+main()
+
